5b5e08c9abb61951655e629509dac1f86684f563
poigarcia <edilitogarcia@gmail.com>
2021-03-08 17:26:42 -0500

    added login modal

---
+++ application/views/modals/login_modal.php
@@ -0,0 +1,49 @@
+<div class="modal fade" id="login_modal" tabindex="-1" role="dialog" aria-labelledby="login_modalLabel">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content">
+			<div class="modal-body">
+				<div id="login_body">
+					<div class="login-top">
+						<ul class="spaced-list between">
+							<li></li>
+							<li><a href="register">Register</a></li>
+						</ul>
+					</div>
+
+					<div class="login-middle">
+						<h1>Log In</h1>
+						<p>Find the freshes vegetables grown by the framers in your community.</p>
+					</div>
+
+					<div class="login-footer">
+						<div class="login-form-body">
+							<form method="post" action="">
+								<div class="form-group">
+									<input type="email" name="email" class="form-control" id="email" placeholder="Email address">
+								</div>
+								<div class="form-group" style="margin-bottom: 5px;">
+									<input type="password" name="password" class="form-control" id="password" placeholder="Email address">
+								</div>
+								<div style="text-align: right;margin-bottom: 10px;">
+									<a href="password-reset" style="font-size: 11px;color: #aaa">Forgot Password?</a>
+								</div>
+								<button class="btn btn-success btn-block">Log In</button>
+							</form>
+						</div>
+
+						<div class="login-with-social">
+							<a href="" class="btn btn-block">
+								<ul class="spaced-list between">
+									<li><i class="fa fa-facebook"></i></li>
+									<li>Facebook</li>
+									<li><i class="fa fa-arrow-right"></i></li>
+								</ul>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
