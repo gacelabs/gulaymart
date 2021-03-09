d879cd0bfb1c1538e21571b93916a3d8a71d3e10
Eddie <gacelabs.inc@gmail.com>
2021-03-09 22:36:08 +0800

    polished

--- application/views/marketplace/product_item_body.php
+++ application/views/marketplace/product_item_body.php
@@ -8,7 +8,7 @@
 		?>
 	</div>
 
-	<div class="hide <?php if ($page_data['is_login'] == 0) {echo "in";} ?>" id="bottom_nav_sm">
+	<!-- <div class="hide <?php if ($page_data['is_login'] == 0) {echo "in";} ?>" id="bottom_nav_sm">
 		<div class="bottom-nav-item <?php if (in_array("marketplace", $body_class)) {echo "active";} ?>" data-toggle="tooltip" data-placement="top" title="Gulaymart"><a href=""><i class="fa fa-leaf"></i></a></div>
 	<?php if ($page_data['is_login'] == 1) : ?>
 		<div class="bottom-nav-item" data-toggle="tooltip" data-placement="top" title="Notifications"><a href="notifications"><i class="fa fa-bell-o"></i></a></div>
@@ -20,5 +20,15 @@
 		<div data-toggle="tooltip" data-placement="top" title="Sign Up" class="bottom-nav-item"><a href="register"><i class="fa fa-user-plus"></i></a></div>
 		<div class="bottom-nav-item" data-toggle="modal" data-target="#login_modal"><span data-toggle="tooltip" data-placement="top" title="Log In"><i class="fa fa-sign-in"></i></span></div>
 	<?php endif ; ?>
+	</div> -->
+
+	<div class="hide <?php if ($page_data['is_login'] == 0) {echo "in";} ?>" id="bottom_nav_sm">
+		<div class="bottom-nav-item <?php if (in_array("marketplace", $body_class)) {echo "active";} ?> onlogged-out-btn<?php echo $current_profile ? ' hide' : '';?>" data-toggle="tooltip" data-placement="top" title="Gulaymart"><a href=""><i class="fa fa-leaf"></i></a></div>
+		<div class="bottom-nav-item onlogged-in-btns<?php echo $current_profile ? '' : ' hide';?>" data-toggle="tooltip" data-placement="top" title="Notifications"><a href="notifications"><i class="fa fa-bell-o"></i></a></div>
+		<div class="bottom-nav-item" data-toggle="modal" data-target="#search_popup"><i class="fa fa-search" data-toggle="tooltip" data-placement="top" title="Search"></i></div>
+		<div data-toggle="tooltip" data-placement="top" title="Sign Up" class="bottom-nav-item onlogged-out-btn<?php echo $current_profile ? ' hide' : '';?>"><a href="register"><i class="fa fa-user-plus"></i></a></div>
+		<div class="bottom-nav-item onlogged-in-btns<?php echo $current_profile ? '' : ' hide';?>" data-toggle="tooltip" data-placement="top" title="Basket"><a href="basket"><i class="fa fa-shopping-basket"></i></a></div>
+		<div class="bottom-nav-item onlogged-in-btns<?php echo $current_profile ? '' : ' hide';?>" data-toggle="tooltip" data-placement="top" title="<?php echo $current_profile ? $current_profile['info']['firstname'] : '';?>"><a href="account"><div class="nav-avatar-body" style="background-image: url('assets/images/avatar.jpg');"></div></a></div>
+		<div class="bottom-nav-item onlogged-out-btn<?php echo $current_profile ? ' hide' : '';?>" data-toggle="modal" data-target="#login_modal"><span data-toggle="tooltip" data-placement="top" title="Log In"><i class="fa fa-sign-in"></i></span></div>
 	</div>
 </div>
