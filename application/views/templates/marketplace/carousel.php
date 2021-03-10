fdb1455a30da62cce0fa0499ca36f17b0be92027
poigarcia <edilitogarcia@gmail.com>
2021-03-10 14:19:51 -0500

    changes

---
+++ application/views/templates/marketplace/carousel.php
@@ -0,0 +1,35 @@
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" id="navbar-carousel_container">
+	<div id="navbar-carousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#navbar-carousel" data-slide-to="0" class="active"></li>
+			<li data-target="#navbar-carousel" data-slide-to="1"></li>
+			<li data-target="#navbar-carousel" data-slide-to="2"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="assets/images/onions.jpg" alt="...">
+				<div class="carousel-caption">
+					<h3>Carousel Title 1</h3>
+					<p>Carousels require the use of an id on the outermost container (the .carousel) for carousel controls</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="assets/images/veg.jpg" alt="...">
+				<div class="carousel-caption">
+					<h3>Carousel Title 2</h3>
+					<p>Carousels require the use of an id on the outermost container (the .carousel) for carousel controls</p>
+				</div>
+			</div>
+			<div class="item">
+				<img src="assets/images/deliver.jpg" alt="...">
+				<div class="carousel-caption">
+					<h3>Carousel Title 3</h3>
+					<p>Carousels require the use of an id on the outermost container (the .carousel) for carousel controls</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
