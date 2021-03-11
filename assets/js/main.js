421d84271496fe183ab4a6fce4f38d0523d1cc4c
Eddie <gacelabs.inc@gmail.com>
2021-03-11 19:54:12 +0800

    put to main js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,22 @@ $(document).ready(function() {
 
 	$('[data-toggle="tooltip"]').tooltip();
 
+	$(document.body).on('click', '.resetpass-btn', function(e) {
+		$('form.sign-in-form').addClass('hide');
+		$('form.resetpass-form').removeClass('hide');
+		$('.ask-sign-in').removeClass('hide');
+		$('.fb-login-btn').addClass('hide');
+		$('.login-detail').addClass('hide');
+		$('.reset-detail').removeClass('hide');
+	});
+
+	$(document.body).on('click', '.ask-sign-in', function(e) {
+		$('form.sign-in-form').removeClass('hide');
+		$('form.resetpass-form').addClass('hide');
+		$('.ask-sign-in').addClass('hide');
+		$('.fb-login-btn').removeClass('hide');
+		$('.login-detail').removeClass('hide');
+		$('.reset-detail').addClass('hide');
+	});
+
 });
