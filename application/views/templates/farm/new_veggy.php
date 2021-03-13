491d82a006d886f868f03faa90992f9fe4ff7e31
poigarcia <edilitogarcia@gmail.com>
2021-03-12 19:13:56 -0500

    dashboard/profile & new-veggy

--- application/views/templates/farm/new_veggy.php
+++ application/views/templates/farm/new_veggy.php
@@ -12,7 +12,6 @@
 					<form action="farm/new-veggy" method="post" enctype="multipart/form-data" class="form-validate" data-ajax="1">
 						<div class="dash-panel-middle">
 							<input type="hidden" name="products[user_id]" value="<?php echo $current_profile['id'];?>">
-<<<<<<< HEAD
 							<div class="form-group">
 								<input type="text" class="form-control" name="products[name]" placeholder="Name" required="required">
 							</div>
@@ -24,8 +23,8 @@
 									<div class="form-group">
 										<select class="form-control" name="products[category_id]" required="required">
 											<option value="">Category</option>
-											<?php if ($current_profile['categories']): ?>
-												<?php foreach ($current_profile['categories'] as $key => $category): ?>
+											<?php if ($this->categories): ?>
+												<?php foreach ($this->categories as $key => $category): ?>
 													<option value="<?php echo $category['id'];?>"><?php echo $category['label'];?></option>
 												<?php endforeach ?>
 											<?php endif ?>
@@ -34,14 +33,14 @@
 								</div>
 								<div class="col-lg-3">
 									<div class="form-group">
-										<input type="number" class="form-control" name="products[stocks]" placeholder="Stocks" required="required">
+										<input type="text" class="form-control" name="products[procedure]" placeholder="Hydrophonic">
 									</div>
 								</div>
 								<div class="col-lg-3">
 									<div class="form-group">
 										<select class="form-control" name="products[location_id]" required="required">
-											<?php if ($current_profile['farms']): ?>
-												<?php foreach ($current_profile['farms'] as $key => $farm): ?>
+											<?php if ($this->farms): ?>
+												<?php foreach ($this->farms as $key => $farm): ?>
 													<option value="<?php echo $farm['location_id'];?>"><?php echo $farm['farm_name'];?></option>
 												<?php endforeach ?>
 											<?php endif ?>
@@ -52,43 +51,28 @@
 							<div class="row">
 								<div class="col-lg-3">
 									<div class="form-group">
-										<input type="number" class="form-control" name="products[old_price]" placeholder="Old Price" required="required">
+										<select class="form-control" name="products[measurement]" required="required">
+											<option value="">Measurement</option>
+											<option value="kg">Kilo</option>
+										</select>
 									</div>
 								</div>
 								<div class="col-lg-3">
 									<div class="form-group">
-										<input type="number" class="form-control" name="products[current_price]" placeholder="Price" required="required">
+										<input type="number" class="form-control" name="products[stocks]" placeholder="Stocks" required="required">
+									</div>
+								</div>
+								<div class="col-lg-3">
+									<div class="form-group">
+										<input type="text" class="form-control" name="products[old_price]" placeholder="Old Price" required="required">
+									</div>
+								</div>
+								<div class="col-lg-3">
+									<div class="form-group">
+										<input type="text" class="form-control" name="products[current_price]" placeholder="Price" required="required">
 									</div>
 								</div>
 							</div>
-=======
-							<input type="text" name="products[name]" placeholder="Name" required="required">
-							<input type="text" name="products[old_price]" placeholder="Old Price" required="required">
-							<input type="text" name="products[current_price]" placeholder="Price" required="required">
-							<textarea name="products[description]" placeholder="Description"></textarea>
-							<input type="text" name="products[procedure]" placeholder="Hydrophonic">
-							<select name="products[measurement]" required="required">
-								<option value="">Measurement</option>
-								<option value="kg">Kilo</option>
-							</select>
-							<input type="number" name="products[stocks]" placeholder="Stocks" required="required">
-							<select name="products[category_id]" required="required">
-								<option value="">Category</option>
-								<?php if ($this->categories): ?>
-									<?php foreach ($this->categories as $key => $category): ?>
-										<option value="<?php echo $category['id'];?>"><?php echo $category['label'];?></option>
-									<?php endforeach ?>
-								<?php endif ?>
-							</select>
-							<label>Farm</label>
-							<select name="products[location_id]" required="required">
-								<?php if ($this->farms): ?>
-									<?php foreach ($this->farms as $key => $farm): ?>
-										<option value="<?php echo $farm['location_id'];?>"><?php echo $farm['farm_name'];?></option>
-									<?php endforeach ?>
-								<?php endif ?>
-							</select>
->>>>>>> 481ce8034984b56c83b78f68161bc7a946c43a69
 						</div>
 						<div class="dash-panel-footer text-right">
 							<ul class="spaced-list between">
