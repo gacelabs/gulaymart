e9a85df52b85e27db68ce076df8a59d96ab35515
poigarcia <edilitogarcia@gmail.com>
2021-04-27 07:38:59 -0400

    update

--- assets/js/basket/checkout.js
+++ assets/js/basket/checkout.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 	$('[js-event="priceTallyPullUp"]').click(function() {
-		$(this).find('i').toggleClass('fa-chevron-right fa-chevron-left');
+		$(this).find('i').toggleClass('fa-angle-right fa-angle-left');
 		$('.price-summary-middle, body.checkout, .order-summary-middle').toggleClass('active');
 	});
 });
