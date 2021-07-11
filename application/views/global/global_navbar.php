d2e462dd62a0dc639924171cc94cfb9c4f128a21
Eddie <gacelabs.inc@gmail.com>
2021-07-11 17:10:17 +0800

    fix++

--- application/views/global/global_navbar.php
+++ application/views/global/global_navbar.php
@@ -18,35 +18,36 @@
 		<div id="global_navbar_grid">
 			<a href=""><i class="fa fa-leaf" id="global_navbar_logo"></i></a>
 
-			<?php if (!in_array("checkout", $middle['body_class'])) : ?>
-			<form action="marketplace/search" method="get" name="search" class="form-validate" id="global_search_form">
-				<div class="input-group">
-					<input type="text" class="form-control" name="keywords" placeholder="Search veggies..." value="<?php echo $data['keywords'];?>">
-					<span class="input-group-btn">
-						<button class="btn btn-default" type="submit" loading-text style="background-color:#fff;"><i class="fa fa-search" style="color:#a8d253;"></i></button>
-					</span>
-				</div>
-			</form>
-			
-			<ul id="global_navbar_btn_list">
-				<?php if ($current_profile) : ?>
-				<li id="nav_basket">
-					<a href="basket/"><i class="fa fa-shopping-basket"></i></a>
-				</li>
-				<?php endif ; ?>
-
-				<li <?php echo(empty($current_profile) ? 'data-toggle="modal" data-target="#login_modal"' : ''); ?>>
+			<?php if (!in_array("checkout", $middle['body_class'])) : 
+				$keywords = isset($data['keywords']) ? $data['keywords'] : '';?>
+				<form action="marketplace/search" method="get" name="search" class="form-validate" id="global_search_form">
+					<div class="input-group">
+						<input type="text" class="form-control" name="keywords" placeholder="Search veggies..." value="<?php echo $keywords;?>">
+						<span class="input-group-btn">
+							<button class="btn btn-default" type="submit" loading-text style="background-color:#fff;"><i class="fa fa-search" style="color:#a8d253;"></i></button>
+						</span>
+					</div>
+				</form>
+				
+				<ul id="global_navbar_btn_list">
 					<?php if ($current_profile) : ?>
-						<a href="profile/">
-							<div id="global_navbar_avatar" style="background-image: url('assets/images/avatar.jpg');"></div>
-							<i class="fa fa-gear global-navbar-badge text-contrast"></i>
-						</a>
-					<?php else : ?>
-						<span class="hidden-xs">Log In <span style="color:#c1c1c1;">|</span> Register</span>
-						<span class="visible-xs"><i class="fa fa-sign-in"></i></span>
+					<li id="nav_basket">
+						<a href="basket/"><i class="fa fa-shopping-basket"></i></a>
+					</li>
 					<?php endif ; ?>
-				</li>
-			</ul>
+
+					<li <?php echo(empty($current_profile) ? 'data-toggle="modal" data-target="#login_modal"' : ''); ?>>
+						<?php if ($current_profile) : ?>
+							<a href="profile/">
+								<div id="global_navbar_avatar" style="background-image: url('assets/images/avatar.jpg');"></div>
+								<i class="fa fa-gear global-navbar-badge text-contrast"></i>
+							</a>
+						<?php else : ?>
+							<span class="hidden-xs">Log In <span style="color:#c1c1c1;">|</span> Register</span>
+							<span class="visible-xs"><i class="fa fa-sign-in"></i></span>
+						<?php endif ; ?>
+					</li>
+				</ul>
 			<?php endif ; ?>
 		</div>
 	</div>
