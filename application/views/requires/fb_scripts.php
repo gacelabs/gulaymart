fa4fa02b59bf7d14397430a8cec9f12caf42a906
Eddie <gacelabs.inc@gmail.com>
2021-07-17 11:46:52 +0800

    fb all good

--- application/views/requires/fb_scripts.php
+++ application/views/requires/fb_scripts.php
@@ -16,35 +16,9 @@
 			if (oUser == false) {
 				switch (status) {
 					case "connected":
-						$('.onlogged-out-btn').addClass('hide');
-						$('.onlogged-in-btns').removeClass('hide');
-
-						$('#login_modal').find('[data-dismiss="modal"]').addClass('hide');
-						$('#login_modal').find('[href="register"]').addClass('hide');
-						$('.login-detail').addClass('hide');
-						$('.login-form-body').addClass('hide');
-						$('.fb-login-btn').addClass('hide');
-						$('.fb-login-panel').removeClass('hide');
-						$('.fb-signing-in').removeClass('hide');
-						$('#login_modal').modal('show');
-
-						/*FB.api('/me?fields=id,email,name', function(data) {
-							// console.log('login 1', fb_acc_response);
-							simpleAjax('authenticate/fb_login', data);
-						});*/
-					break;
-					case "not_authorized": case "unknown":
-						$('.onlogged-out-btn').removeClass('hide');
-						$('.onlogged-in-btns').addClass('hide');
-						
-						$('#login_modal').removeAttr('data-keyboard').removeAttr('data-backdrop');
-						$('#login_modal').find('[data-dismiss="modal"]').removeClass('hide');
-						$('#login_modal').find('[href="register"]').removeClass('hide');
-						$('.login-detail').removeClass('hide');
-						$('.login-form-body').removeClass('hide');
-						$('.fb-login-btn').removeClass('hide');
-						$('.fb-login-panel').addClass('hide');
-						$('.fb-signing-in').addClass('hide');
+						FB.logout(function(response) {
+							fb_acc_response = response;
+						});
 					break;
 				}
 			} else {
