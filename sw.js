e47fefd5f196dc93cec5a1453baa7b68800a6a40
Eddie <gacelabs.inc@gmail.com>
2021-07-23 09:25:11 +0800

    reverted

--- sw.js
+++ sw.js
@@ -1,13 +1,13 @@
 self.addEventListener('install', function(event) {
-	console.log("installed", event);
+	console.log("installed");
 });
 
 self.addEventListener('activate', function(event) {
-	console.log("activated", event);
+	console.log("activated");
 });
 
 self.addEventListener('fetch', function(event) {
-	console.log("fetched", event);
+	console.log("fetched");
 	/*event.respondWith(
 		fetch(event.request).catch(function() {
 			return caches.match(event.request);
