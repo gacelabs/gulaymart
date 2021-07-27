0af2f745d30e3a13e5ca43b65a9f6d47bd11c76e
Eddie <gacelabs.inc@gmail.com>
2021-07-27 11:12:05 +0800

    polished

--- assets/js/orders/main.js
+++ assets/js/orders/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 	$('[js-event="showOrderFooter"]').click(function() {
 		$(this).find('i.fa').toggleClass('fa-angle-down fa-angle-up');
-		$('.order-footer-farm, .order-footer-payment').toggleClass('hidden-xs');
+		$(this).parents('[js-element]:first').find('.order-footer-farm, .order-footer-payment').toggleClass('hidden-xs');
 	});
 });
 
