db6f14a8956f3390b0c2846a69c56bdb8e4f2cba
Eddie <gacelabs.inc@gmail.com>
2021-07-28 08:45:49 +0800

    test

--- sw.js
+++ sw.js
@@ -12,5 +12,24 @@ self.addEventListener('fetch', function(event) {
 		fetch(event.request).catch(function() {
 			return caches.match(event.request);
 		})
-	);*/
+		);*/
+	});
+
+self.addEventListener('notificationclick', function(event) {
+	console.log('On notification click: ', event.notification.tag);
+	event.notification.close();
+
+	/*This looks to see if the current is already open and*/
+	/*focuses if it is*/
+	event.waitUntil(clients.matchAll({
+		type: "window"
+	}).then(function(clientList) {
+		for (var i = 0; i < clientList.length; i++) {
+			var client = clientList[i];
+			if (client.url == '/' && 'focus' in client)
+				return client.focus();
+		}
+		if (clients.openWindow)
+			return clients.openWindow('/');
+	}));
 });
