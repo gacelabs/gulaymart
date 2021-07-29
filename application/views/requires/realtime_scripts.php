c778ae94a72eefd9298813a1640169cd3a1972fc
Eddie <gacelabs.inc@gmail.com>
2021-07-29 17:05:29 +0800

    test

--- application/views/requires/realtime_scripts.php
+++ application/views/requires/realtime_scripts.php
@@ -3,6 +3,7 @@
 	var realtime = false, serviceWorker, isSubscribed;
 	window.initSendData = function() {
 		realtime = new SendData({
+			debug: true,
 			afterInit: function() {
 				realtime.connect(function() {
 					/*console.log('gulaymart.com ready to communicate!');*/
