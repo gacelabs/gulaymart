6239ab9637e703f28348a97919b672e8324c061f
Eddie <gacelabs.inc@gmail.com>
2021-08-01 09:57:21 +0800

    test

--- assets/js/global.js
+++ assets/js/global.js
@@ -24,6 +24,32 @@ $(document).ready(function() {
 	}
 });
 
+let deferredPrompt;
+document.getElementById('add-pwa').addEventListener('click', async () => {
+	/*Hide the app provided install promotion*/
+	document.getElementById('add-pwa').style.display = 'none';
+	/*Show the install prompt*/
+	deferredPrompt.prompt();
+	/*Wait for the user to respond to the prompt*/
+	const { outcome } = await deferredPrompt.userChoice;
+	/*Optionally, send analytics event with outcome of user choice*/
+	console.log(`User response to the install prompt: ${outcome}`);
+	/*We've used the prompt, and can't use it again, throw it away*/
+	deferredPrompt = null;
+});
+
+/*Initialize deferredPrompt for use later to show browser install prompt.*/
+window.addEventListener('beforeinstallprompt', (e) => {
+	/*Prevent the mini-infobar from appearing on mobile*/
+	e.preventDefault();
+	/*Stash the event so it can be triggered later.*/
+	deferredPrompt = e;
+	/*Update UI notify the user they can install the PWA*/
+	document.getElementById('add-pwa').style.display = 'block';
+	/*Optionally, send analytics event that PWA install promo was shown.*/
+	console.log(`'beforeinstallprompt' event was fired.`, e);
+});
+
 function setCookie(cname, cvalue, exdays) {
 	var d = new Date();
 	d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
