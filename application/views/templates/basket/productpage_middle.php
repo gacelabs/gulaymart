761bb128cb243f399baab3a5db4cb0bf1b5a3995
Eddie <gacelabs.inc@gmail.com>
2021-08-04 20:09:36 +0800

    test

--- application/views/templates/basket/productpage_middle.php
+++ application/views/templates/basket/productpage_middle.php
@@ -62,14 +62,15 @@
 											</div>
 										</div>
 									<?php endforeach ?>
-								<?php else: ?>
+								<?php elseif (!$current_profile): ?>
 									Be the first to make a feedback. <button type="button" class="btn btn-xs btn-contrast" onclick="$('#buy_now_btn').click();">Buy Now!</button>
 								<?php endif ?>
 								<?php if ($can_comment == 0 AND $current_profile): ?>
-								<div class="comment-box" style="margin-left:-15px;margin-right:-15px;">
-									<hr style="margin-bottom:10px;border-color:#d0d0d0;">
-									<?php $this->view('looping/comment_item', ['placeholder'=>'Write a feedback ...', 'under'=>0, 'page'=>$product]); ?>
-								</div>
+									Your comments are important, please feel free to add what you desire.
+									<div class="comment-box" style="margin-left:-15px;margin-right:-15px;">
+										<hr style="margin-bottom:10px;border-color:#d0d0d0;">
+										<?php $this->view('looping/comment_item', ['placeholder'=>'Write a feedback ...', 'under'=>0, 'page'=>$product]); ?>
+									</div>
 								<?php endif ?>
 
 							</div>
