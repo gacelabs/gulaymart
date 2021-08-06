f37c516c6bc3db09e0d7be253ea75b6c82b74d8a
poigarcia <edilitogarcia@gmail.com>
2021-08-06 16:23:12 -0400

    added mobile install landing page

--- assets/landing/js/landing.js
+++ assets/landing/js/landing.js
@@ -1,16 +1,18 @@
 $(document).ready(function() {
 
 	var img = ['1.jpg', '2.jpg', '3.jpg', '0.jpg'],
-		phrase = ['farmer\'s feed their family.', 'farmers dream big.', 'farmers live for a day.', 'farmers to produce more.'],
+		phrase = ['feed their family.', 'dream big.', 'live for a day.', 'to produce more.'],
 		sec = 4000,
 		i = 0;
 
-	setInterval(function(e) {
+	setInterval(function() {
+		$('#subphrase').hide();
+
 		if (i < img.length) {
 			$('#first').css({
 				'background-image': 'url(./images/'+img[i]+')'
 			});
-			$('#subphrase').fadeOut('fast').text(phrase[i]).fadeIn('slow');
+			$('#subphrase').text(phrase[i]).fadeIn('fast');
 	 		i++;
 		}
 	}, sec);
