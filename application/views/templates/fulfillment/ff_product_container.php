14e47c257d984e5c3189e70d8c0c176458a35482
Eddie <gacelabs.inc@gmail.com>
2021-08-10 07:48:22 +0800

    test

--- application/views/templates/fulfillment/ff_product_container.php
+++ application/views/templates/fulfillment/ff_product_container.php
@@ -35,7 +35,7 @@
 								<!-- per order -->
 								<?php
 									$row_cnt++;
-									if ($order['status'] == GM_CANCELLED_STATUS AND !in_array($data['status'], ['placed','cancelled'])) continue;
+									// if ($order['status'] == GM_CANCELLED_STATUS AND !in_array($data['status'], ['placed','cancelled'])) continue;
 									$photo_url = 'https://via.placeholder.com/50x50.png?text=No+Image';
 									$product = $order['product'];
 									if ($product['photos'] AND isset($product['photos']['main'])) {
