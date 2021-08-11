103a77eb2ebc8c16c7031d2f6ac338e206de4c1e
poigarcia <edilitogarcia@gmail.com>
2021-08-11 17:55:05 -0400

    updated css

--- application/views/looping/veggy_card.php
+++ application/views/looping/veggy_card.php
@@ -1,6 +1,6 @@
 <div class="veggy-nearby-product-item">
 	<a href="<?php echo $data['product_url'];?>">
-		<div class="veggy-nearby-product-img" style="background-image: url('<?php identify_main_photo($data);?>'); background-size: 228px 268px;">
+		<div class="veggy-nearby-product-img" style="background-image: url('<?php identify_main_photo($data);?>');>
 			<div class="veggy-nearby-product-body">
 				<p class="zero-gaps"><i class="fa fa-map-marker"></i><?php if ($data['durationval'] == 0): ?> Right Away<?php else: ?> About <?php echo $data['duration'];?><?php endif ?></p>
 			</div>
