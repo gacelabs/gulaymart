48afbf3c409f51fbc84a76a7c2f7bd55d6736a63
poigarcia <edilitogarcia@gmail.com>
2021-08-16 17:04:54 -0400

    update

--- assets/js/marketplace/main.js
+++ assets/js/marketplace/main.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
 	loadMore($('#load_more_btn'));
+
+	$('#veggy_categories').animate({
+		scrollLeft: $('.veggy-category-item-inner.active').offset().left
+	},1000);
+
 });
